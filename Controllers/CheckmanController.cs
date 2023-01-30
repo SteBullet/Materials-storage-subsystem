dee3974f254bcfd7d6f39c31aadcc1936ab47bf9
NoBlackIr <nicitina.nis@gmail.com>
2023-01-30 17:45:00 +0300

    функционал добавления складов и функционал учета материалов в табеле

--- Controllers/CheckmanController.cs
+++ Controllers/CheckmanController.cs
@@ -55,6 +55,60 @@ namespace Materials_storage_subsystem.Controllers
         }
 
         [HttpGet]
+        public IActionResult ExpenseSheetEdit(int id)
+        {
+            ExpenseSheetEditModel Model = new ExpenseSheetEditModel();
+            Model.ExpenseSheet = _context.ExpenseSheets.Include(x => x.Expenses).ThenInclude(x => x.Material).First(x => x.Id == id);
+            Model.Materials = _context.Materials.ToList();
+            return View(Model);
+        }
+
+        [HttpPost]
+        public IActionResult AddExpense(ExpenseSheetEditModel Model)
+        {
+            Model.MaterialMovement.ExpenseSheet = _context.ExpenseSheets.First(x => x.Id == Model.MaterialMovement.ExpenseSheetId);
+            Model.MaterialMovement.Material = _context.Materials.First(x => x.Id == Model.MaterialMovement.MaterialId);
+            MaterialRemaining materialRemaining = _context.MaterialRemainings.FirstOrDefault(x => x.WarehouseId == Model.MaterialMovement.ExpenseSheet.WarehouseId && x.MaterialId == Model.MaterialMovement.MaterialId);
+            if (materialRemaining == null)
+            {
+                materialRemaining = new MaterialRemaining
+                {
+                    Material = Model.MaterialMovement.Material,
+                    MaterialId = Model.MaterialMovement.MaterialId,
+                    Quantity = Model.MaterialMovement.Quantity,
+                    Warehouse = Model.MaterialMovement.ExpenseSheet.Warehouse,
+                    WarehouseId = Model.MaterialMovement.ExpenseSheet.WarehouseId
+                };
+                _context.MaterialRemainings.Add(materialRemaining);
+            }
+            else
+            {
+                materialRemaining.Quantity += Model.MaterialMovement.Quantity;
+                _context.MaterialRemainings.Update(materialRemaining);
+            }
+            _context.MaterialMovements.Add(Model.MaterialMovement);
+            _context.SaveChanges();
+            return Redirect($"/Checkman/ExpenseSheetEdit/{Model.MaterialMovement.ExpenseSheetId}");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteExpense(int id)
+        {
+            var materialMovement = _context.MaterialMovements.First(m => m.Id == id);
+            _context.MaterialMovements.Remove(materialMovement);
+            _context.SaveChanges();
+            return Redirect($"/Checkman/ExpenseSheetEdit/{materialMovement.ExpenseSheetId}");
+        }
+
+        [HttpPost]
+        public IActionResult ExpenseSheetEdit(ExpenseSheet expenseSheet)
+        {
+            _context.ExpenseSheets.Update(expenseSheet);
+            _context.SaveChanges();
+            return View("ExpenseSheetList", _context.ExpenseSheets.ToList());
+        }
+
+        [HttpGet]
         public IActionResult ExpenseSheetCreate()
         {
             var WarehousesId = _context.Warehouses.Select(w => w.Id).ToList();
